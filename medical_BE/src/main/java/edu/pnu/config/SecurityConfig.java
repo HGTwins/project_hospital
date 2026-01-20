f817a54f82297748b034153a1a2be69e2ba3dd71
maybecocheon <guswl293@gmail.com>
2026-01-20 12:29:06 +0900

    oauth2 test

--- medical_BE/src/main/java/edu/pnu/config/SecurityConfig.java
+++ medical_BE/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
 	private CorsConfigurationSource corsSource() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000", "http://140.245.77.74:8080",
-												"https://nonefficient-lezlie-progressively.ngrok-free.dev"));
+												"http://mediguide.duckdns.org:8080"));
 		config.addAllowedMethod(CorsConfiguration.ALL);
 		config.addAllowedHeader(CorsConfiguration.ALL);
 		config.setAllowCredentials(true);
