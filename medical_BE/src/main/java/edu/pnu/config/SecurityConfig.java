d94a0ef3540d51cee13cd128a4966da8313a5098
maybecocheon <guswl293@gmail.com>
2026-01-21 17:51:04 +0900

    Update SecurityConfig.java

--- medical_BE/src/main/java/edu/pnu/config/SecurityConfig.java
+++ medical_BE/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
 	
 	private CorsConfigurationSource corsSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000",
+		config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000", "http://10.125.121.179:3000",
 												"https://project-emergency.vercel.app", "https://nonefficient-lezlie-progressively.ngrok-free.dev"));
 		config.addAllowedMethod(CorsConfiguration.ALL);
 		config.addAllowedHeader(CorsConfiguration.ALL);
